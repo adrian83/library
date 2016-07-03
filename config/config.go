bd6794e512cc3ee629cf20d3352efc3b3abea9cd
adrian83 <adrianbrzoza1983@gmail.com>
2016-07-03 22:34:55 +0200

    config creation

--- config/config.go
+++ config/config.go
@@ -2,4 +2,23 @@ package config
 
 import ()
 
-type Config struct{}
+type Config struct {
+	server  ServerConfig  `json:"server"`
+	session SessionConfig `json:"session"`
+}
+
+type SessionConfig struct {
+	Redis    RedisConfig `json:"redis"`
+	IDLength int         `json:"idLength"`
+}
+
+type RedisConfig struct {
+	DB       int    `json:"db"`
+	Password string `json:"password"`
+	Host     string `json:"host"`
+	Port     int    `json:"port"`
+}
+
+type ServerConfig struct {
+	Port int `json:"port"`
+}
