0ccfe16669fbd3f52809a1e30aeb1e4de64461ad
adrian83 <adrianbrzoza1983@gmail.com>
2019-12-26 14:39:01 +0100

    [fe] cleaning

--- library-fe/web/main.dart
+++ library-fe/web/main.dart
@@ -1,6 +1,6 @@
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
-import 'package:todo_fe/app_component.template.dart' as ng;
+import 'package:library_fe/app_component.template.dart' as ng;
 
 import 'package:http/http.dart';
 
